9133062da3af3cf3bd07624a3d1786928bc0e423
EngelOfChipolata <engelofdeath@live.fr>
2016-10-27 21:50:07 +0200

    Ajoute la classe d'association ListItem

---
+++ Courses/app/src/main/java/com/example/nicolas/shoptic/core/ListItem.java
@@ -0,0 +1,37 @@
+package com.example.nicolas.shoptic.core;
+
+/**
+ * Association class between a list and a product. Used to represent the many to many association
+ * and to add details to this association (number of product added and units)
+ *
+ * Created by guilhem on 27/10/16.
+ */
+
+public class ListItem {
+    /**
+     * Units available in order to count occurence of a product inside a list
+     */
+    public enum ItemUnit{
+        PIECE,
+        DIZAINE,
+        LITRE,
+        GRAMME,
+        KILOGRAMME,
+        PACK,
+        PAQUET,
+        DOUZAINE,
+        BOUTEILLE
+    }
+
+    private int quantity;
+    private ItemUnit unit;
+    private Product product;
+    private List list;
+
+    public ListItem(int quantity, ItemUnit unit, Product product, List list) {
+        this.quantity = quantity;
+        this.unit = unit;
+        this.product = product;
+        this.list = list;
+    }
+}
