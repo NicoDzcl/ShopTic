6b6d1b70d1163d1bfc65c0eec00f2c770898d260
Nicolas <nicolas.dzieciol@gmail.com>
2016-11-09 21:54:00 +0100

    Première notification

--- Courses/app/src/main/java/com/example/nicolas/shoptic/NotifierActivity.java
+++ Courses/app/src/main/java/com/example/nicolas/shoptic/NotifierActivity.java
@@ -1,9 +1,13 @@
 package com.example.nicolas.shoptic;
 
 import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,16 +15,18 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class NotifierActivity extends AppCompatActivity implements
         View.OnClickListener {
 
-    Button btnDatePicker, btnTimePicker;
+    Button btnDatePicker, btnTimePicker, btnSave;
     EditText txtDate, txtTime;
     private int mYear, mMonth, mDay, mHour, mMinute;
     private String array_spinner[];
+    private String array_spinner2[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +35,33 @@ public class NotifierActivity extends AppCompatActivity implements
 
         btnDatePicker=(Button)findViewById(R.id.btn_date);
         btnTimePicker=(Button)findViewById(R.id.btn_time);
+        btnSave = (Button)findViewById(R.id.buttonSave);
+
 
         btnDatePicker.setOnClickListener(this);
         btnTimePicker.setOnClickListener(this);
         array_spinner = new String[]{"Une seule fois", "Hebdomadaire", "Mensuelle", "Annuelle"};
+        array_spinner2 = new String[]{"Notification", "E-mail"};
         Spinner s = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter adapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_item, array_spinner);
         s.setAdapter(adapter);
+        Spinner s2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter adapter2 = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, array_spinner2);
+        s2.setAdapter(adapter2);
 
+        btnSave.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getBaseContext(), "Ajout d'une notification", Toast.LENGTH_SHORT).show();
+                createNotification();
+            }
+        });
     }
+
 
     @Override
     public void onClick(View v) {
@@ -85,5 +108,36 @@ public class NotifierActivity extends AppCompatActivity implements
                     }, mHour, mMinute, false);
             timePickerDialog.show();
         }
+    }
+
+    private final void createNotification(){
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_menu_share)
+                        .setContentTitle("ShopTic")
+                        .setContentText("Vous devez faire vos courses!");
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // Because clicking the notification opens a new ("special") activity, there's
+        // no need to create an artificial back stack.
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+
+// Sets an ID for the notification
+        int mNotificationId = 001;
+// Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+
     }
 }
